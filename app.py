43bcbe3059d61a311a06052ca19a12fdfa6c8bc2
Jackson Jesse <jacksonjess2205@gmail.com>
2025-11-13 09:03:32 +0530

    trying

--- app.py
+++ app.py
@@ -7,21 +7,18 @@ app = Flask(__name__)
 with open('rf_model_test.pkl', 'rb') as f:
     model = pickle.load(f)
 
-accuracy = 0.7597402597402597
-
-@app.route('/predict', methods = ['GET'])
+@app.route('/predict', methods=[GET])
 def predict():
 
-    features = [6, 90, 54, 24, 0, 14.77, 0.34, 30]
-
+    features = [6, 90, 234, 115, 66.7, 45, 54, 98]
     input_features = np.array(features).reshape(1, -1)
-    prediction = model.predict(input_features)[0]
 
+    prediction = model.predict(input_features)[0]
+    
     if prediction == 1:
         return jsonify({'prediction': 'Diabetic'})
-        
     else:
-        return jsonify({'prediction': 'Non-Diabetic'})
+        return jsonift({'prediction': 'Non-Diabetic'})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
